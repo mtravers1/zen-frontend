877c5b71747f97dcb777f4d04e638d6a46b4e266
NIKHIL SHARMA <nikhilsharma101001@gmail.com>
2025-07-09 05:10:08 +0530

    improve: content padding hero 1 section

--- src/app/sections/HomeSection.tsx
+++ src/app/sections/HomeSection.tsx
@@ -8,55 +8,57 @@ export function HomeSection() {
     <div className="relative w-full">
       <Element id="home" name="home" className="element">
         {/* Hero Section 1 */}
-        <div className="relative aspect-[16/9] w-full justify-center flex flex-col md:flex-row items-center">
+        <div className="relative aspect-[16/9] w-full justify-center flex flex-col md:flex-row items-center h-full">
           {/* Background Image for Hero Section 1 */}
           <div className="absolute inset-0 w-full h-full">
             <Image
               src="/Hero-Banner-Woman-sky.png"
               alt=""
               fill
-              className="object-cover object-[25%] md:object-center"
+              className="object-cover object-[0%] md:object-center"
               priority
             />
           </div>
 
-          {/* Left Side - Text Content */}
-          <div className="relative z-10 w-full md:w-1/2 flex justify-start items-center p-4 md:p-8 pb-0">
-            <div className="max-w-2xl flex flex-col items-start text-left">
-              <h1 className="font-bold text-white text-2xl sm:text-5xl md:text-6xl leading-tight mb-6 md:mb-8">
-                <span className="block">Your CFO</span>
-                <span className="block">at the touch</span>
-                <span className="block">of a button.</span>
-              </h1>
+          <div className="ps-[6vw] pe-[2vw] relative mx-auto justify-center flex flex-col md:flex-row items-center w-full">
+            {/* Left Side - Text Content */}
+            <div className="relative z-10 w-full md:w-1/2 flex justify-start items-center p-4 md:p-8 pb-0">
+              <div className="max-w-2xl flex flex-col items-start text-left">
+                <h1 className="font-bold text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight mb-2 md:mb-8">
+                  <span className="block">Your CFO</span>
+                  <span className="block">at the touch</span>
+                  <span className="block">of a button.</span>
+                </h1>
+              </div>
             </div>
-          </div>
 
-          {/* Right Side - App Store Buttons */}
-          <div className="relative z-10 w-full md:w-1/2 flex justify-start md:justify-end items-center p-4 md:p-8">
-            <div className="flex flex-col gap-2 w-full max-w-[250px]">
-              <Link
-                href="https://play.google.com/store/apps/details?id=com.zentavos.mobile"
-                className="relative hover:opacity-90 transition-opacity h-8 md:h-16 w-28 md:w-auto"
-              >
-                <Image
-                  src="/Google_Play_Store_badge_EN (1).svg"
-                  alt="Get on Google Play"
-                  fill
-                  className="object-contain w-fit"
-                />
-              </Link>
+            {/* Right Side - App Store Buttons */}
+            <div className="relative z-10 w-full md:w-1/2 flex justify-start md:justify-end items-center p-4 md:p-8">
+              <div className="flex flex-col gap-2 w-full max-w-[250px]">
+                <Link
+                  href="https://play.google.com/store/apps/details?id=com.zentavos.mobile"
+                  className="relative hover:opacity-90 transition-opacity h-8 md:h-12 lg:h-16 w-28 md:w-[250px]"
+                >
+                  <Image
+                    src="/Google_Play_Store_badge_EN (1).svg"
+                    alt="Get on Google Play"
+                    fill
+                    className="object-contain w-fit"
+                  />
+                </Link>
 
-              <Link
-                href="https://apps.apple.com/us/app/zentavos/id6742329325"
-                className="relative hover:opacity-90 transition-opacity h-10 md:h-16 w-28 md:w-auto"
-              >
-                <Image
-                  src="/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
-                  alt="Download on the App Store"
-                  fill
-                  className="object-contain"
-                />
-              </Link>
+                <Link
+                  href="https://apps.apple.com/us/app/zentavos/id6742329325"
+                  className="relative hover:opacity-90 transition-opacity h-10 md:h-14 lg:h-18 w-28 md:w-[250px]"
+                >
+                  <Image
+                    src="/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+                    alt="Download on the App Store"
+                    fill
+                    className="object-contain"
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>

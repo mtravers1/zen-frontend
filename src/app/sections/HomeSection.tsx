1e390a717bb17f1283022e10392861d66e6b4ee0
NIKHIL SHARMA <nikhilsharma101001@gmail.com>
2025-07-09 06:15:48 +0530

    improve: hero 2 section

--- src/app/sections/HomeSection.tsx
+++ src/app/sections/HomeSection.tsx
@@ -8,7 +8,7 @@ export function HomeSection() {
     <div className="relative w-full">
       <Element id="home" name="home" className="element">
         {/* Hero Section 1 */}
-        <div className="relative aspect-[16/9] w-full justify-center flex flex-col md:flex-row items-center h-full">
+        <div className="hero-1 relative aspect-[16/9] w-full justify-center flex flex-col md:flex-row items-center h-full max-h-[80vh]">
           {/* Background Image for Hero Section 1 */}
           <div className="absolute inset-0 w-full h-full">
             <Image
@@ -24,7 +24,7 @@ export function HomeSection() {
             {/* Left Side - Text Content */}
             <div className="relative z-10 w-full md:w-1/2 flex justify-start items-center p-4 md:p-8 pb-0">
               <div className="max-w-2xl flex flex-col items-start text-left">
-                <h1 className="font-bold text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight mb-2 md:mb-8">
+                <h1 className="font-bold text-white text-[4vmin] sm:text-4xl md:text-5xl lg:text-6xl leading-tight mb-2 md:mb-8">
                   <span className="block">Your CFO</span>
                   <span className="block">at the touch</span>
                   <span className="block">of a button.</span>
@@ -64,41 +64,37 @@ export function HomeSection() {
         </div>
 
         {/* Hero Section 2 */}
-        <div className="hero-2 relative w-full min-h-screen py-16 md:py-0">
-          <div className="container mx-auto h-full flex items-center px-4">
-            <div className="w-full flex flex-col lg:flex-row items-center relative z-10">
-              {/* Text Content */}
-              <div className="w-full lg:w-1/2 text-center lg:text-right mb-12 lg:mb-0 lg:pr-12">
-                <h2 className="font-bold text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight">
-                  <div className="mb-3 sm:mb-4 md:mb-6">Accounting.</div>
-                  <div className="mb-3 sm:mb-4 md:mb-6">Analytics.</div>
-                  <div className="mb-3 sm:mb-4 md:mb-6">Advice.</div>
-                  <div>Access.</div>
-                </h2>
-              </div>
-            </div>
-            {/* Image Content */}
-            <div className="w-full h-full">
-              {/* Green Background */}
-              <div className="image-bg absolute inset-0 rounded-2xl overflow-hidden">
-                <Image
-                  src="/Zentavos_green_bg.png"
-                  alt=""
-                  fill
-                  className="object-cover object-center"
-                />
-              </div>
+        <div className="hero-2 relative w-full aspect-[16/9] flex items-center justify-center  mb-[6vh] md:mb-[10vh] lg:mb-[16vh] 2xl:mb-[20vh] ps-[6vw] pe-[2vw] max-h-[80vh]">
+          <div className="w-full flex flex-col items-center relative z-10">
+            {/* Text Content */}
+            <h2 className="font-bold text-white text-[4vmin] sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl leading-tight mb-2 md:mb-8 text-left">
+              <div>Accounting.</div>
+              <div>Analytics.</div>
+              <div>Advice.</div>
+              <div>Access.</div>
+            </h2>
+          </div>
+          {/* Green Background */}
+          <div className="image-bg absolute inset-0 overflow-hidden h-[90%]">
+            <Image
+              src="/Zentavos_green_bg.png"
+              alt=""
+              fill
+              className="object-cover object-center blur-[3px]"
+            />
+          </div>
 
-              {/* Phone Image */}
-              <div className="image-fg absolute -top-4 sm:-top-8 -left-4 sm:-left-8 w-full h-full">
-                <Image
-                  src="/thumbnail_Hero_Phone_screenshot_improved_shadow.png"
-                  alt="Zentavos Mobile App"
-                  fill
-                  className="object-contain object-bottom"
-                  priority
-                />
-              </div>
+          {/* Image Content */}
+          <div className="w-full h-full relative max-w-[70vh] max-h-[70vh]">
+            {/* Phone Image */}
+            <div className="image-fg absolute w-full h-full ">
+              <Image
+                src="/thumbnail_Hero_Phone_screenshot_improved_shadow.png"
+                alt="Zentavos Mobile App"
+                fill
+                className="object-contain mt-6 md:mt-16 scale-220"
+                priority
+              />
             </div>
           </div>
         </div>
